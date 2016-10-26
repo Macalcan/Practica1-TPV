ad90644f5f1656957fce41420885bd3c9c60b057
Usuario Local <Usuario Local>
2016-10-26 12:24:18 +0200

    Declaramos las funciones draw y loadFile, corregimos load

--- HolaSDL/TexturasSDL.cpp
+++ HolaSDL/TexturasSDL.cpp
@@ -5,40 +5,42 @@ using namespace std;
 
 TexturasSDL::TexturasSDL()
 {
+	
+
 }
 //--------------------------------------------------------------------------------//
-bool TexturasSDL::load(SDL_Renderer* pRenderer, string const& nombArch, SDL_Color const& colKey)
+bool TexturasSDL::load(SDL_Renderer* pRenderer, string const& nombArch)
 {
 	//El archivo de imagen que se carga
-	string imgName {nombArch};//supuesta ruta de la imagen
 	SDL_Surface* pTempSurface = nullptr; //puntero a una superficie en la que se va a dibujar
 	bool carga = true; //la imagen se ha cargado correctamente
 	//Carga de la imagen
-	pTempSurface = SDL_LoadBMP(imgName.c_str()); //carga la superficie del archivo
+	pTempSurface = loadFile(nombArch); //carga la superficie del archivo
 	if (pTempSurface == nullptr)
 	{
-		cout << "Imposible cargar el archivo" << imgName << "! \nSDL Error: " << SDL_GetError() << "\n";
+		cout << "Imposible cargar el archivo" << nombArch << "! \nSDL Error: " << SDL_GetError() << "\n";
 		carga = false;
 	}
 	else 
 	{
-		//pTexture = SDL_CreateTextureFromSurface(pRenderer, pTempSurface);
+		pTextura = SDL_CreateTextureFromSurface(pRenderer, pTempSurface);
 		SDL_FreeSurface(pTempSurface);//liberamos la superficie porque se ha cargado la textura
 		carga = pTextura != nullptr;
 	}
-	/*if (loadFile(nombArch) == nullptr)
-	{
-		cout << "Imposible cargar el archivo" << imgName << "! \nSDL Error: " << SDL_GetError() << "\n";
-		carga = false;
-	}
-	else 
-	{
-		carga = pTextura != nullptr;
-	}*/
 	return carga;
 }
 //--------------------------------------------------------------------------------//
-void TexturasSDL::draw(SDL_Renderer* pRenderer, SDL_Rect const& rect){}
+void TexturasSDL::draw(SDL_Renderer* pRenderer, SDL_Rect const& rect){
+	SDL_RenderCopy(pRenderer, pTextura, &rect, winRect);
+}
+//--------------------------------------------------------------------------------//
+SDL_Surface* TexturasSDL::loadFile(string file)
+{
+	SDL_Surface* archivo = nullptr;
+	archivo = SDL_LoadBMP(file.c_str());
+	return archivo;
+
+}
 //--------------------------------------------------------------------------------//
 TexturasSDL::~TexturasSDL()
 {
--- HolaSDL/TexturasSDL.h
+++ HolaSDL/TexturasSDL.h
@@ -6,12 +6,10 @@ public:
 	TexturasSDL();
 	~TexturasSDL();
 private: 
-	bool load(SDL_Renderer* pRenderer, string const& nombArch, SDL_Color const& colKey);
+	bool load(SDL_Renderer* pRenderer, string const& nombArch);
 	SDL_Surface* loadFile(string file);
 	void draw(SDL_Renderer* pRenderer, SDL_Rect const& rect);
 	SDL_Texture* pTextura;
-	const int TEXTURE_WIDTH = 100;   //texture dimension
-	const int TEXTURE_HEIGHT = 100;  //texture dimension
-
+	const SDL_Rect* winRect;
 };
 
